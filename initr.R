54c48d2830053a4775cbc8cd85147a49910d1af6
Klinton Bicknell <kbicknell@northwestern.edu>
2015-03-19 12:26:36 -0500

    added R script to initr

---
+++ initr.R
@@ -0,0 +1,33 @@
+## first update currently installed packages
+update.packages(ask = FALSE)
+
+## next install CRAN packages
+install_if_not_already_cran <- function(package_list) {
+  installed <-
+    suppressMessages(
+      suppressWarnings(
+        sapply(package_list, require, quietly = TRUE,
+               warn.conflicts = FALSE, character.only = TRUE)))
+  
+  to_install <- package_list[!installed]
+  sapply(to_install, install.packages)
+  invisible()
+}
+
+install_if_not_already_cran(c(
+  'devtools',
+  'doParallel',
+  'dplyr',
+  'ggplot2',
+  'ggthemes',
+  'Hmisc',
+  'lme4',
+  'stringr',
+  'tidyr'
+))
+
+## last, use devtools for github packages
+devtools::install_github(c(
+  'kbicknell/lmer.slimr',
+  'langcomp/ggintervals'
+))
